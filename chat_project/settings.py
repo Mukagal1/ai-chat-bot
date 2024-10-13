81419330cf13d4ffc95dea0123788f1b20a776e5
Mukagal1 <mukagaliturmaganbetov@gmail.com>
2024-10-13 21:03:40 +0600

    fix allowed hosts

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -30,7 +30,7 @@ OPENAI_API_KEY = env('OPENAI_API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ai-chat-bot-ig9m.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
